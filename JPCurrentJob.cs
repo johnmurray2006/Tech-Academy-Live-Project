65ba57f8f75825fe0816ccdd2a216b60977c46fa
john murray <42979559+johnmurray2006@users.noreply.github.com>
2018-12-09 10:29:00 -0700

    Story 2911

---
+++ JPCurrentJob.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JobPlacementDashboard.Models
+{
+    public class JPCurrentJob
+    {
+
+        [Key]
+        
+
+        public Guid JPCurrentId { get; set; }
+        [DisplayName("Company Name")]
+        public string JPCompanyName { get; set; }
+        [DisplayName("Job Title")]
+        public string JPJobTitle { get; set; }
+        [DisplayName("Job Category")]
+        public JPJobCategory JPJobCategory { get; set; }
+        [DisplayName("Salary")]
+        public decimal JPSalary { get; set; }
+        [DisplayName("Work Location City")]
+        public string JPCompanyCity { get; set; }
+        [DisplayName("Work Location State")]
+        public JPCompanyState JPCompanyState { get; set; }
+        [DisplayName("Careers Page")]
+        public string JPCareersPage { get; set; }
+        public string ApplicationUserId { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
